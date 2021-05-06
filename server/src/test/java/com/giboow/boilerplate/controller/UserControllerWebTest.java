baf425795bad58808ae23479a089bd8ef466ec4e
Philippe Gibert <philippe.gibert@gmail.com>
2021-05-06 23:25:31 +0200

    add github action sctipt for test CI
    test add postgresdb
    
    WIP : Review test with postgres
    
    WIP : github-action => Review gralde test
    
    WIP : github-action => Review gralde test
    
    WIP : github-action => Review gralde test
    
    WIP : github-action => Review gralde test

--- server/src/test/java/com/giboow/boilerplate/controller/UserControllerWebTest.java
+++ server/src/test/java/com/giboow/boilerplate/controller/UserControllerWebTest.java
@@ -11,6 +11,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -18,6 +23,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.security.web.header.Header;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -31,6 +37,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest
 //@Import(SpringSecurityWebAuxTestConfig.class)
 @Import({SpringSecurityWebAuxTestConfig.class, WebSecurity.class, AppSercurityConfig.class})
+@ActiveProfiles("test")
 public class UserControllerWebTest {
 
     @Autowired
